4069c8519b07d3d77d209b8da1b4e58fb1ef2af6
L. Palharês Rosa <luciano.palhares@enesolucoes.com.br>
2025-01-17 10:16:20 -0300

    update auth in sellers and sales routes

--- routes/api.php
+++ routes/api.php
@@ -8,22 +8,18 @@ use App\Http\Controllers\AuthController;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::prefix('sellers')->group(function () {
-    Route::post('/', [SellersController::class, 'createSeller']);
-    Route::get('/', [SellersController::class, 'getAllSellers']);
-});
-
-Route::prefix('sales')->group(function () {
-    Route::post('/', [SalesController::class, 'createSale']);
-    Route::get('{id}', [SalesController::class, 'getSalesBySeller']);
-});
-
 Route::middleware('auth:api')->group(function () {
     Route::get('user', [AuthController::class, 'getUser']);
     Route::post('logout', [AuthController::class, 'logout']);
 
+    Route::prefix('sellers')->group(function () {
+        Route::post('/', [SellersController::class, 'createSeller']);
+        Route::get('/', [SellersController::class, 'getAllSellers']);
+    });
 
-
-
+    Route::prefix('sales')->group(function () {
+        Route::post('/', [SalesController::class, 'createSale']);
+        Route::get('{id}', [SalesController::class, 'getSalesBySeller']);
+    });
 });
 
