996994b036f3b6ed45ab42b326abd3a57e5ab086
L. Palharês Rosa <luciano.palhares@enesolucoes.com.br>
2025-01-18 19:25:03 -0300

    rabitmq and email

---
+++ app/Mail/SendEmailSales.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendEmailSales extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $emailData;
+
+    public function __construct($emailData)
+    {
+        $this->emailData = $emailData;
+    }
+
+    public function build()
+    {
+        return $this->view('emails.sales')
+                    ->with('data', $this->emailData);
+    }
+}
---
+++ resources/views/emails/sales.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Email</title>
+</head>
+<body>
+    <h1>Hello, {{ $data['name'] }}!</h1>
+    <p>Welcome to our platform!</p>
+</body>
+</html>
